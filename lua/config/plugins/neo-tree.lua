15d2a5596c72fd33d6b20fe07f5f4e064889677d
randy115 <randyb11595@hotmail.com>
2024-12-20 15:16:22 -0600

    lazy loading neo-tree commands

--- lua/config/plugins/neo-tree.lua
+++ lua/config/plugins/neo-tree.lua
@@ -8,9 +8,9 @@ return {
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    config = function()
-      vim.keymap.set("n", "<leader>b", "<cmd>Neotree toggle<CR>")
-      vim.keymap.set("n", "<leader>q", "<cmd>Neotree close<CR>")
-    end,
+    keys = {
+      { "<leader>b", "<cmd>Neotree toggle<CR>" },
+      { "<leader>q", "<cmd>Neotree close<CR>" }
+    },
   },
 }
