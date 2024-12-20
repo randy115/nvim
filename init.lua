9d0ae38966df8ed2bf191e98360f13a44c2e0be1
randy115 <randyb11595@hotmail.com>
2024-12-19 22:31:39 -0600

    adding neo-tree

--- init.lua
+++ init.lua
@@ -1,4 +1,23 @@
+-- setting map leader to space
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- bootstrapping lazy to vim
 require("config.lazy")
+
+-- vim configs
 vim.opt.shiftwidth = 4
-vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
+vim.g.have_nerd_font = true
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
