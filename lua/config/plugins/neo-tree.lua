17b2e8e8f309b482d324f3cd0e44cf76fd0c8287
randy115 <randyb11595@hotmail.com>
2025-11-02 22:58:29 -0600

    adding namespaces to neotree and replacing namespace for telescope from s to f

--- lua/config/plugins/neo-tree.lua
+++ lua/config/plugins/neo-tree.lua
@@ -9,8 +9,9 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
-      vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>")
-      vim.keymap.set("n", "<leader>q", "<cmd>Neotree close<CR>")
-    end
+      vim.keymap.set("n", "<leader>eo", "<cmd>Neotree toggle<CR>")
+      vim.keymap.set("n", "<leader>eq", "<cmd>Neotree close<CR>")
+      vim.keymap.set("n", "<leader>ef", "<cmd>Neotree focus<CR>")
+    end,
   },
 }
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -20,15 +20,16 @@ return {
 
       require("telescope").load_extension("fzf")
 
-      vim.keymap.set("n", "<leader>sp", require("telescope.builtin").lsp_document_symbols)
-      vim.keymap.set("n", "<space>sh", require("telescope.builtin").help_tags)
-      vim.keymap.set("n", "<space>sf", require("telescope.builtin").find_files)
-      vim.keymap.set("n", "<space>ss", function()
+      vim.keymap.set("n", "<leader>fp", require("telescope.builtin").lsp_document_symbols)
+      vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
+      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>ss", function()
         require("telescope.builtin").find_files({
           cwd = vim.fn.stdpath("config"),
         })
       end)
-      vim.keymap.set("n", "<space>sl", function()
+      vim.keymap.set("n", "<leader>fl", function()
         require("telescope.builtin").find_files({
           cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
         })
