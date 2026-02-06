0ef95b7326b6c861ce50973bedd4f3d177ef5266
rui <randyb11595@hotmail.com>
2026-02-05 23:26:38 -0600

    fixing keymaps

--- lua/config/plugins/tmux-navigator.lua
+++ lua/config/plugins/tmux-navigator.lua
@@ -1,10 +1,5 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
-    vim.keymap.set("n", "<C-Space>h", "<cmd><C-U>TmuxNavigateLeft<cr>"),
-    vim.keymap.set("n", "<C-Space>j", "<cmd><C-U>TmuxNavigateDown<cr>"),
-    vim.keymap.set("n", "<C-Space>k", "<cmd><C-U>TmuxNavigateUp<cr>"),
-    vim.keymap.set("n", "<C-Space>l", "<cmd><C-U>TmuxNavigateRight<cr>"),
-    vim.keymap.set("n", "<C-Space>n", "<cmd><C-U>TmuxNavigatePrevious<cr>"),
   },
 }
